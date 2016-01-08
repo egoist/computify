cf4a54ce67af1770cfcea9264378ac091427c351
EGOIST <0x142857@gmail.com>
2016-01-08 11:16:56 +0800

    make it more safe

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ module.exports = function (str) {
 	// if you can control what will be passed to `eval`
 	// it won't be as evil as you have heard
 	var evalIsNotEvil = eval
-	var re = /[a-zA-Z]+/g
-	var fineUnits = str.match(re)
+	var unitRe = /[a-zA-Z]+/
+	var stripRe = /[^\d\+\-\*\/\%]/g
+	var fineUnits = str.match(unitRe)
 	var unit = fineUnits && fineUnits[0]
-	var stripedStr = str.replace(re, '')
+	var stripedStr = str.replace(stripRe, '')
 	if (unit) {
 		return evalIsNotEvil(stripedStr) + unit
 	}
